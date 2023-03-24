8efe2e084e199344daea560201a84034e2eefde6
Aruan-creator <aruan.kot3@gmail.com>
2023-03-24 20:42:00 +0600

    added form and filter

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import Todo from "./components/Todo";
 
 function App(props) {
   const taskList = props.tasks.map((task) => (
-  <Todo id={task.id} name={task.name} completed={task.completed}/>
-));
+  <Todo 
+    id={task.id} 
+    name={task.name} 
+    completed={task.completed}
+    key={task.id}
+    />
+  )
+);
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -48,7 +54,7 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <Todo />
+        {taskList}
       </ul>
     </div>
   );
